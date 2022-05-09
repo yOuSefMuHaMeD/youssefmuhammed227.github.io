eb8a20a87e35e3c40f98ba630bb1a96135910ca2
youssefmuhammed227 <105038126+youssefmuhammed227@users.noreply.github.com>
2022-05-09 23:04:24 +0200

    Add files via upload

---
+++ Final Project - Copy/js/script.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function() {
+  AOS.init();
+});
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+  navToggler.classList.toggle("active");
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("open");
+  if (nav.classList.contains("open")) {
+    nav.style.maxHeight = nav.scrollHeight + "px";
+  } else {
+    nav.removeAttribute("style");
+  }
+}
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
